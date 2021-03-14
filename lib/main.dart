06f13f92f6850f1c7a9205c5f0908cc61f10d641
Sivan Wolberg <sivan@wolberg.pro>
2021-03-14 21:58:34 +0200

    working on chanllanges

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,15 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: [
+        Expanded(
+          child: Image.asset('images/dise2.png'),
+        ),
+        Expanded(
+          child: Image.asset('images/dise1.png'),
+        ),
+      ],
+    );
   }
 }
