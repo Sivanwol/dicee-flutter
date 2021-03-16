d34ce1f501ce6c06ba576c14fd556764e528517a
Sivan Wolberg <sivan@wolberg.pro>
2021-03-16 14:54:55 +0200

    finished up challenge code

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -16,7 +17,45 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+enum DiceDirection { Left, Right }
+
+class _DicePageState extends State<DicePage> {
+  int numberDiceL = 1;
+  int numberDiceR = 1;
+  @override
+  void initState() {
+    super.initState();
+    numberDiceL = getRandomDiceNumber();
+    numberDiceR = getRandomDiceNumber();
+    print('int value: $numberDiceL');
+    print('int value: $numberDiceR');
+  }
+
+  getRandomDiceNumber() {
+    return (new Random()).nextInt(6) + 1;
+  }
+
+  onDicePressButton(DiceDirection direction) {
+    setState(() {
+      switch (direction) {
+        case DiceDirection.Left:
+          numberDiceL = getRandomDiceNumber();
+
+          print('int value: $numberDiceL');
+          break;
+        case DiceDirection.Right:
+          numberDiceR = getRandomDiceNumber();
+          print('int value: $numberDiceR');
+          break;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -28,14 +67,22 @@ class DicePage extends StatelessWidget {
               flex: 1,
               child: Container(
                 padding: EdgeInsets.all(5.0),
-                child: Image.asset('images/dice2.png'),
+                child: TextButton(
+                    onPressed: () {
+                      onDicePressButton(DiceDirection.Left);
+                    },
+                    child: Image.asset('images/dice$numberDiceL.png')),
               ),
             ),
             Expanded(
               flex: 1,
               child: Container(
                   padding: EdgeInsets.all(5.0),
-                  child: Image.asset('images/dice1.png')),
+                  child: TextButton(
+                      onPressed: () {
+                        onDicePressButton(DiceDirection.Right);
+                      },
+                      child: Image.asset('images/dice$numberDiceR.png'))),
             ),
           ],
         ),
--- tasks.md
+++
@@ -1,9 +0,0 @@
-# General Tasks Todo
-
-here list of general task for the project (it will automatically change on the fly)
-
-# Task Challenge 1
-
-- [x] Add padding into Dice image 1
-- [x] Add padding into Dice image 2
-- [x] make build and upload firebase hosting
